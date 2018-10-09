577cf1078e9be6f48824e3621dc5f0cf8731b119
JakeSteinebronn <jakesteinebronn@gmail.com>
2018-10-09 16:34:06 -0400

    Things lots of things make code more yay

--- TeamCode/src/main/java/ImuTesting/AutoTest.java
+++ TeamCode/src/main/java/ImuTesting/AutoTest.java
@@ -14,6 +14,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import org.firstinspires.ftc.robotcore.external.navigation.Position;
 import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
@@ -30,7 +31,7 @@ public class AutoTest extends LinearOpMode {
     public DcMotor motorR;
     public Servo lServo;
     public Servo rServo;
-    public DistanceSensor sensor1;
+    //public DistanceSensor sensor1;
     public DistanceSensor sensor2;
 
     BNO055IMU imu;
@@ -38,18 +39,40 @@ public class AutoTest extends LinearOpMode {
     Orientation angles;
     Acceleration gravity;
 
+    enum InitState {
+        HANGING,
+        STARTPOS,
+        DELAY,
+        CRATER,
+        READY;
+
+
+        public InitState getNext(){
+            return this.ordinal()<InitState.values().length-1?InitState.values()[this.ordinal()+1]:InitState.READY;
+        }
+    }
+    InitState initState=InitState.HANGING;
+
+
     enum State{
-        MOVE,
-        TURN,
-        MOVE2,
+        DETACH,
+        MOVEBIT,
+        FACEWALL,
+        DELAYTIME,
+        GOTOWALL,
+        FACEDEPOT,
+        GOTOWALL2,
+        DEPOSIT,
+        FACECRATER,
+        DRIVECRATER,
         STOP;
 
         public State getNext(){
-            return this.ordinal()<State.values().length-1?State.values()[this.ordinal()+1]:State.MOVE;
+            return this.ordinal()<State.values().length-1?State.values()[this.ordinal()+1]:State.DETACH;
         }
 
     }
-    State state=State.MOVE;
+    State state=State.DETACH;
 
     int encoderTickInitial=0;
     double headingInitial=0;
@@ -64,10 +87,9 @@ public class AutoTest extends LinearOpMode {
         return (ticks>0?motorR.getCurrentPosition()-encoderTickInitial>=ticks:motorR.getCurrentPosition()-encoderTickInitial<=ticks);
     }
     private boolean turnDegrees(double power, int degrees){
-        motorR.setPower(-power);
-        motorL.setPower(-power);
-        return (double)angles.firstAngle-headingInitial>=degrees;
-
+        motorR.setPower((degrees>0?-1:1)*power);
+        motorL.setPower((degrees>0?-1:1)*power);
+        return (degrees>0?(double)angles.firstAngle-headingInitial>=degrees:(double)angles.firstAngle-headingInitial<=degrees);
 
 
     }
@@ -94,6 +116,13 @@ public class AutoTest extends LinearOpMode {
         motorL.setPower(0);
         motorR.setPower(0);
     }
+
+    boolean startHanging=false;
+    boolean startNearCrater=true;
+    boolean redCrater=true;
+    int timeDelay=3;
+
+
     @Override
     public void runOpMode() throws InterruptedException {
         motorL = hardwareMap.get(DcMotor.class, "leftMotor");
@@ -106,7 +135,7 @@ public class AutoTest extends LinearOpMode {
         lServo.setDirection(Servo.Direction.REVERSE);
         rServo.setDirection(Servo.Direction.FORWARD);
 
-        sensor1=hardwareMap.get(DistanceSensor.class, "s1");
+        //sensor1=hardwareMap.get(DistanceSensor.class, "s1");
         sensor2=hardwareMap.get(DistanceSensor.class, "s2");
 
 
@@ -128,9 +157,109 @@ public class AutoTest extends LinearOpMode {
         imu.initialize(parameters);
         imu.startAccelerationIntegration(new Position(), new Velocity(), 1000);
 
-        composeTelemetry();
 
+        boolean selection=false;
+        boolean dDown=false;
+        boolean dUp=false;
+        boolean aDown=false;
+
+        boolean isReady=false;
+        /*
+        while(!isReady){
+
+            if(gamepad1.dpad_down){dDown=true;}
+            if(gamepad1.dpad_up){dUp=true;}
+            if(gamepad1.a){aDown=true;}
+
+            switch(initState){
+                case HANGING:
+                    telemetry.clearAll();
+                    telemetry.addData("Start Hanging?","");
+                    telemetry.addData("Yes",selection?"<":" ");
+                    telemetry.addData("No",!selection?"<":" ");
+                    if(!gamepad1.dpad_down&&dDown){
+                        selection=!selection;
+                        dDown=false;
+                    }
+                    if(!gamepad1.dpad_up&&dUp){
+                        dUp=false;
+                        selection=!selection;
+                    }
+                    if(!gamepad1.a&&aDown){
+                        aDown=false;
+                        initState=initState.getNext();
+                        startHanging=selection;
+                    }
+
+                    break;
+                case DELAY:
+                    telemetry.clearAll();
+                    telemetry.addData("Set Delay(Seconds)","");
+                    telemetry.addData("Delay time:",timeDelay);
+                    if(!gamepad1.dpad_down&&dDown){
+                        timeDelay--;
+                        dDown=false;
+                    }
+                    if(!gamepad1.dpad_up&&dUp){
+                        dUp=false;
+                        timeDelay++;
+                    }
+                    if(!gamepad1.a&&aDown){
+                        aDown=false;
+                        initState=initState.getNext();
+                    }
+
+                    break;
+                case STARTPOS:
+                    telemetry.clearAll();
+                    telemetry.addData("Start facing crater?","");
+                    telemetry.addData("Yes",selection?"<":" ");
+                    telemetry.addData("No",!selection?"<":" ");
+                    if(!gamepad1.dpad_down&&dDown){
+                        selection=!selection;
+                        dDown=false;
+                    }
+                    if(!gamepad1.dpad_up&&dUp){
+                        dUp=false;
+                        selection=!selection;
+                    }
+                    if(!gamepad1.a&&aDown){
+                        aDown=false;
+                        initState=initState.getNext();
+                        startNearCrater=selection;
+                    }
+                    break;
+                case CRATER:
+                    telemetry.clearAll();
+                    telemetry.addData("Aim for own crater?","");
+                    telemetry.addData("Yes",selection?"<":" ");
+                    telemetry.addData("No",!selection?"<":" ");
+                    if(!gamepad1.dpad_down&&dDown){
+                        selection=!selection;
+                        dDown=false;
+                    }
+                    if(!gamepad1.dpad_up&&dUp){
+                        dUp=false;
+                        selection=!selection;
+                    }
+                    if(!gamepad1.a&&aDown){
+                        aDown=false;
+                        initState=initState.getNext();
+                        redCrater=selection;
+                    }
+                    break;
+                case READY:
+                default:
+                    isReady=true;
+                    break;
+
+
+            }
+        }
+        */
         waitForStart();
+
+        composeTelemetry();
 
         while(!isStopRequested()){
             telemetry.update();
@@ -139,25 +268,69 @@ public class AutoTest extends LinearOpMode {
 
 
             switch(state){
-                case MOVE:
-                    if(driveTicks(.5,5000)){
+                case DETACH:
+                    if(startHanging){
+
+                    }else {
                         next();
                     }
-
-
                     break;
-                case TURN:
-                    if(turnDegrees(.5,45)){
+                case MOVEBIT:
+                    if(driveTicks(.2,-1500)){
                         next();
+
                     }
-
-
                     break;
-                case MOVE2:
-                    if(driveTicks(.5,- 7000)){
+                case FACEWALL:
+                    if(startNearCrater){
+                        if(turnDegrees(.2,45)){
+                            next();
+                        }
+                    }else{
+                        if(turnDegrees(.2,-45)){
+
+                        }
+                    }
+                    break;
+                case DELAYTIME:
+                    Thread.sleep(timeDelay*1000);
+                    next();
+                    break;
+                case GOTOWALL:
+                    motorDrive(-.2);
+                    if(sensor2.getDistance(DistanceUnit.CM)<20){
                         next();
                     }
-
+                    break;
+                case FACEDEPOT:
+                    if(turnDegrees(.2, 90)){
+                        next();
+                    }
+                    break;
+                case GOTOWALL2:
+                    motorDrive(-.2);
+                    if(sensor2.getDistance(DistanceUnit.CM)<20){
+                        next();
+                    }
+                    break;
+                case DEPOSIT:
+                    if(driveTicks(.3,500)){
+                        next();
+                    }
+                    break;
+                case FACECRATER:
+                    if(redCrater&&turnDegrees(.2,180)){
+                        next();
+                    }else{
+                        if(turnDegrees(.2,90)){
+                            next();
+                        }
+                    }
+                    break;
+                case DRIVECRATER:
+                    if(driveTicks(.2, -10000)){
+                        next();
+                    }
                     break;
                 case STOP:
                 default:
